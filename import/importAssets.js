be2407147762c12b1f1e3f9082e78d23f518191c
MikazukiMizuki <winstonak13112001@gmail.com>
2022-06-14 23:10:49 +0700

    update import asset

--- import/importAssets.js
+++ import/importAssets.js
@@ -1,20 +1,23 @@
-import Story from '../assets/BackgroundStory.jpg'
-import Vision from '../assets/BackgroundVision.jpg'
-import Mission from '../assets/BackgroundMission.jpg'
-import Founder from '../assets/BackgroundFounder.jpg'
-import BgLogin from '../assets/BackgroundLogin.jpg';
-import BgRegister from '../assets/BackgroundRegister.jpg';
-import BgMain from '../assets/BackgroundMain.jpg';
-import Rising from '../assets/BackgroundRising.jpg';
-import Discount from '../assets/BackgroundDiscount.jpg';
+import Story from "../assets/BackgroundStory.jpg";
+import Vision from "../assets/BackgroundVision.jpg";
+import Mission from "../assets/BackgroundMission.jpg";
+import Founder from "../assets/BackgroundFounder.jpg";
+import BgLogin from "../assets/BackgroundLogin.jpg";
+import BgRegister from "../assets/BackgroundRegister.jpg";
+import BgMain from "../assets/BackgroundMain.jpg";
+import Rising from "../assets/BackgroundRising.jpg";
+import Discount from "../assets/BackgroundDiscount.jpg";
+import Logo from "../assets/logo.png";
+
 export {
-    Story, 
-    Vision, 
-    Mission,
-    Founder,
-    BgLogin,
-    BgRegister,
-    BgMain,
-    Rising,
-    Discount,
+  Story,
+  Vision,
+  Mission,
+  Founder,
+  BgLogin,
+  BgRegister,
+  BgMain,
+  Rising,
+  Discount,
+  Logo,
 };
